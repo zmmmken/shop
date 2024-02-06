6476ec85fa65c1616508c86a9136221cbd524388
mohammad.kenevisi <mohammad.kenevisi78@gmail.com>
2024-02-06 21:57:12 +0330

    (#6)define product args in viewModel

--- feature/product/src/main/java/com/kenevisi/product/ProductViewModel.kt
+++ feature/product/src/main/java/com/kenevisi/product/ProductViewModel.kt
@@ -1,5 +1,6 @@
 package com.kenevisi.product
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.kenevisi.product.presentation.ProductAction
 import com.kenevisi.product.presentation.ProductSideEffect
@@ -12,7 +13,11 @@ import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class ProductViewModel @Inject constructor() : ViewModel() {
+class ProductViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+    private val args by lazy {  ProductFragmentArgs.fromSavedStateHandle(savedStateHandle) }
+
     private val _uiState = MutableStateFlow(ProductUiState())
     val uiState = _uiState.asStateFlow()
 
