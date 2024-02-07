d8a15935ab288a9d1777dfaba9894ad07b5646a6
mohammad.kenevisi <mohammad.kenevisi78@gmail.com>
2024-02-07 17:24:38 +0330

    (#8) develop GetProductUseCase

---
+++ domain/src/main/java/com/kenevisi/domain/usecases/GetProductUseCase.kt
@@ -0,0 +1,19 @@
+package com.kenevisi.domain.usecases
+
+import com.kenevisi.domain.base.SuspendUseCase
+import com.kenevisi.domain.contract.ProductEntity
+import com.kenevisi.domain.contract.ProductRepository
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+class GetProductUseCase @Inject constructor(
+    private val productRepository: ProductRepository
+) : SuspendUseCase<GetProductUseCase.Params, ProductEntity>(Dispatchers.IO) {
+
+    override suspend fun execute(input: Params): ProductEntity {
+        return productRepository.getProductInfo(input.productKey)
+    }
+
+    data class Params(val productKey: String)
+
+}
