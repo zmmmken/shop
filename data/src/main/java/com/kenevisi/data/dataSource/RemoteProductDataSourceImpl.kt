39955c93117397b1a443124135c74f10abcc31de
mohammad.kenevisi <mohammad.kenevisi78@gmail.com>
2024-02-07 14:15:31 +0330

    (#8)add ErrorResponse

--- data/src/main/java/com/kenevisi/data/dataSource/RemoteProductDataSourceImpl.kt
+++ data/src/main/java/com/kenevisi/data/dataSource/RemoteProductDataSourceImpl.kt
@@ -1,13 +1,17 @@
 package com.kenevisi.data.dataSource
 
 import com.kenevisi.data.api.ProductApiRemoteService
+import com.kenevisi.data.extensions.bodyOrThrow
 import com.kenevisi.data.responses.ProductResponse
+import kotlinx.serialization.json.Json
 import javax.inject.Inject
 
 class RemoteProductDataSourceImpl @Inject constructor(
-    private val remoteService: ProductApiRemoteService
+    private val remoteService: ProductApiRemoteService,
+    private val json: Json
 ) : RemoteProduceDataSource {
-    override suspend fun getProduct(productKey:String): ProductResponse {
-        return remoteService.getProduct(productKey).body()!!
+    override suspend fun getProduct(productKey: String): ProductResponse {
+        return remoteService.getProduct(productKey).bodyOrThrow(json)
     }
 }
+
