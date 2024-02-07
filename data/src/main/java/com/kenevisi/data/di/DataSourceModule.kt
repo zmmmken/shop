25afa0ec38d043eb608ee1cb54e7db0ce5715f72
mohammad.kenevisi <mohammad.kenevisi78@gmail.com>
2024-02-07 13:40:29 +0330

    (#8)create dataSourceModule

---
+++ data/src/main/java/com/kenevisi/data/di/DataSourceModule.kt
@@ -0,0 +1,21 @@
+package com.kenevisi.data.di
+
+import com.kenevisi.data.dataSource.RemoteProduceDataSource
+import com.kenevisi.data.dataSource.RemoteProductDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindProductRemoteDataSource(
+        dataSource: RemoteProductDataSourceImpl
+    ): RemoteProduceDataSource
+}
