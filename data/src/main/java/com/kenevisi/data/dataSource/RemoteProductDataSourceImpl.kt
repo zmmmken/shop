ba52ae3bffc2bf9ed7c3a2a6f6de8169c5168935
mohammad.kenevisi <mohammad.kenevisi78@gmail.com>
2024-02-07 13:35:00 +0330

    (#8)add inject constructor

--- data/src/main/java/com/kenevisi/data/dataSource/RemoteProductDataSourceImpl.kt
+++ data/src/main/java/com/kenevisi/data/dataSource/RemoteProductDataSourceImpl.kt
@@ -2,8 +2,9 @@ package com.kenevisi.data.dataSource
 
 import com.kenevisi.data.api.ProductApiRemoteService
 import com.kenevisi.data.responses.ProductResponse
+import javax.inject.Inject
 
-class RemoteProductDataSourceImpl(
+class RemoteProductDataSourceImpl @Inject constructor(
     private val remoteService: ProductApiRemoteService
 ) : RemoteProduceDataSource {
     override suspend fun getProduct(productId:String): ProductResponse {
