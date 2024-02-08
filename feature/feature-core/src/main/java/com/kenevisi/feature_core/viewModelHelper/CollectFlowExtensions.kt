d945ad31c3321a4d6f6dc586285d2ae9953c038c
mohammad.kenevisi <mohammad.kenevisi78@gmail.com>
2024-02-08 10:27:01 +0330

    (#10) add CollectFlowExtensions.kt

---
+++ feature/feature-core/src/main/java/com/kenevisi/feature_core/viewModelHelper/CollectFlowExtensions.kt
@@ -0,0 +1,50 @@
+package com.kenevisi.feature_core.viewModelHelper
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+
+fun <T> Fragment.collectOnEachStart(flow: Flow<T>, collector: suspend (T) -> Unit): Job {
+    return lifecycleScope.launch {
+        viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collector)
+        }
+    }
+}
+
+fun <T> Fragment.collectOnEachStart(
+    flow: MutableStateFlow<T>,
+    collector: suspend (T) -> Unit
+): Job {
+    return lifecycleScope.launch {
+        viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collector)
+        }
+    }
+}
+
+fun <T> Fragment.collectOnEachResume(flow: Flow<T>, collector: suspend (T) -> Unit): Job {
+    return lifecycleScope.launch {
+        viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+            flow.collectLatest(collector)
+        }
+    }
+}
+
+fun <T> Fragment.collectOnEachResume(
+    flow: MutableStateFlow<T>,
+    collector: suspend (T) -> Unit
+): Job {
+    return lifecycleScope.launch {
+        viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+            flow.collectLatest(collector)
+        }
+    }
+}
