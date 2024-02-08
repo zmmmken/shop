224d3f5bc2a78fb413ecadb182111747b6e6aec2
mohammad.kenevisi <mohammad.kenevisi78@gmail.com>
2024-02-08 14:13:34 +0330

    (#10) fix locale

--- app/src/main/java/com/kenevisi/torobenterance/MainActivity.kt
+++ app/src/main/java/com/kenevisi/torobenterance/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.kenevisi.torobenterance
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kenevisi.core.exceptions.AppLanguages
+import com.kenevisi.core.exceptions.setAppLocale
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +12,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(newBase.setAppLocale(AppLanguages.FARSI))
     }
 }
