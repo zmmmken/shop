adbdea82bd336ec0dd8a9c9f4647623011a328a9
mohammad.kenevisi <mohammad.kenevisi78@gmail.com>
2024-02-08 10:27:55 +0330

    (#10) fix GetSimilarProductsUseCase

--- domain/src/main/java/com/kenevisi/domain/usecases/GetSimilarProductsUseCase.kt
+++ domain/src/main/java/com/kenevisi/domain/usecases/GetSimilarProductsUseCase.kt
@@ -6,16 +6,15 @@ import com.kenevisi.domain.base.PagingDataUseCase
 import com.kenevisi.domain.contract.ProductEntity
 import com.kenevisi.domain.contract.ProductRepository
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetSimilarProductsUseCase @Inject constructor(
     private val productRepository: ProductRepository,
     @IoDispatcher private val dispatcher: CoroutineDispatcher
-) : PagingDataUseCase<GetProductUseCase.Params, ProductEntity>(dispatcher) {
+) : PagingDataUseCase<GetSimilarProductsUseCase.Params, ProductEntity>(dispatcher) {
 
-    override fun execute(input: GetProductUseCase.Params): Flow<PagingData<ProductEntity>> {
+    override fun execute(input: Params): Flow<PagingData<ProductEntity>> {
         return productRepository.getSimilarProducts(input.productKey)
     }
 
